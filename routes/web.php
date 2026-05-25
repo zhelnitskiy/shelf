62df777b3f546b4bbb63a5c5b0c5f9696778a492
Serega Zhelnitskiy <zhelnitskiy@hostpro.ua>
2026-05-25 18:32:42 +0300

    refactor: clean Laravel scaffold for API-only application

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'name' => config('app.name'),
+        'status' => 'ok',
+    ]);
 });
